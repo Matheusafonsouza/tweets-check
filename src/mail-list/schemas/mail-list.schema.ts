59d25d452da7a882adb3775dd9fb6778120de7fb
matheusafonsouza <matheusafonsouza@gmail.com>
2022-02-16 02:27:35 -0300

    mail list schema

--- src/mail-list/schemas/mail-list.schema.ts
+++ src/mail-list/schemas/mail-list.schema.ts
@@ -1,1 +1,15 @@
-export class MailList {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+export type MailListDocument = MailList & Document;
+
+@Schema()
+export class MailList {
+  @Prop({ type: MongooseSchema.Types.ObjectId })
+  id: string;
+
+  @Prop({ type: [String] })
+  emails: string[];
+}
+
+export const MailListSchema = SchemaFactory.createForClass(MailList);
